72ed2de8fd65e5e49858557ce8f8f445e86c21b8
dennis-tech <hedenniskibe@gmail.com>
2024-08-06 16:05:08 +0300

    meeting room development

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,18 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster"
 
+import '@stream-io/video-react-sdk/dist/css/styles.css'
 
 
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Yoom",
-  description: "Zoom clone",
+  title: "YOOM",
+  description: "Video calling app",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
 };
 
 export default function RootLayout({
